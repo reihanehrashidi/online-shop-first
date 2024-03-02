6597d36706cbefbc7d64371ab1a2b198344fe294
reihanehrashidi <reihanehrashidi2181@gmail.com>
2024-03-01 16:35:36 -0800

    general blueprint

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+from blueprints.general import app as general
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+app.register_blueprint(general)
 
 if __name__ == "__main__":
     app.run()
